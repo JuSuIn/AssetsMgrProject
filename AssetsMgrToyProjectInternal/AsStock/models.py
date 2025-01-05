135b9875dbb6d4968d59bea20079b14fa015d66e
SUINJU <jusuin5876@naver.com>
2025-01-05 19:35:56 +0900

    자산관리 앱 한국 주식 관련 Api 항목 Model 정리( Asset management app Korean stock related API item model summary)

--- AssetsMgrToyProjectInternal/AsStock/models.py
+++ AssetsMgrToyProjectInternal/AsStock/models.py
@@ -1,3 +1,4 @@
 from django.db import models
+from models.api.api_models import KospiDDInFor, KosDaQDDInFor
 
 # Create your models here.
---
+++ AssetsMgrToyProjectInternal/AsStock/models/api/api_models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+#Kospi 
+#코스피 시리즈 지수의 시세정보를 제공(일별)에 대한 모델
+class KospiDDInFor(models.Model):
+    bas_dd = models.CharField(max_length=8) #기준일자
+    inx_clss = models.CharField(max_length=100) #계열구분
+    idx_nm = models.CharField(max_length=100)#지수명
+    clsprc_idx = models.BigIntegerField()#종가
+    cmpprevdd_idx = models.BigIntegerField()#대비
+    fluc_rt = models.FloatField()#등락률
+    opnprc_idx = models.BigIntegerField()#시가
+    hgprc_idx = models.BigIntegerField()#고가
+    lwprc_idx = models.BigIntegerField()#저가
+    acc_trdvol = models.BigIntegerField()#거래량
+    acc_trdval = models.BigIntegerField()#거래대금
+    mktcap = models.BigIntegerField()#상장시가총액
+    
+
+#Koddaq 
+#코스닥시장에 상장되어 있는 주권의 매매정보
+class KosDaQDDInFor(models.Model):
+        bas_dd = models.CharField(max_length=8) #기준일자
+        isu_cd = models.CharField(max_length=100) #종목코드 
+        isu_nm = models.CharField(max_length=100)#종목명
+        mkt_nm = models.CharField(max_length=100)#시장구분
+        sect_tp_nm = models.CharField(max_length=100)#소속부
+        tdd_clsprc = models.BigIntegerField()#종가
+        cmpprevdd_prc = models.BigIntegerField()#대비
+        fluc_rt = models.FloatField()#등락률
+        tdd_opnprc = models.BigIntegerField()#시가
+        tdd_hgprc = models.BigIntegerField()#고가
+        tdd_lwprc = models.BigIntegerField()#저가
+        acc_trdvol = models.BigIntegerField()#거래량
+        acc_trdval = models.BigIntegerField()#거래대금
+        mktcap = models.BigIntegerField()#시가총액
+        list_shrs = models.BigIntegerField()#상장주식수
+        
